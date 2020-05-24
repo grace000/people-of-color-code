153fa59b7982da2c09f7d661ca7f87b422532e44
Tiffani Elmore <tiffani.grace.elmore@gmail.com>
2020-05-24 13:20:55 -0500

    update author description info

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `People of Color Code`,
+    description: `People of Color Code Website`,
+    author: `POC Code Team`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
