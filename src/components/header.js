ef5fba74125af30ee44b28fa231a177594cefb00
Tiffani Elmore <tiffani.grace.elmore@gmail.com>
2020-08-26 04:22:42 -0500

    responsive header style

--- src/components/header.js
+++ src/components/header.js
@@ -1,21 +1,53 @@
 import PropTypes from "prop-types"
-import React from "react"
+import React, { useState, useEffect } from "react"
 
-const Header = ({ navigation }) => (
-  <header>
-    <div>
-      <h1 style={{ margin: 0 }}>
-        <ul className="header-list">
+const Header = ({ navigation }) => {
+  const [isNavVisible, setNavVisibility] = useState(false)
+  const [isSmallScreen, setIsSmallScreen] = useState(false)
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia("(max-width: 1024px)")
+    mediaQuery.addListener(handleMediaQueryChange)
+    handleMediaQueryChange(mediaQuery)
+
+    return () => {
+      mediaQuery.removeListener(handleMediaQueryChange)
+    }
+  }, [])
+
+  const handleMediaQueryChange = mediaQuery => {
+    if (mediaQuery.matches) {
+      setIsSmallScreen(true)
+    } else {
+      setIsSmallScreen(false)
+    }
+  }
+
+  const toggleNav = () => {
+    setNavVisibility(!isNavVisible)
+  }
+
+  return (
+    <header className="header">
+      <h3 className="logo">LOGO</h3>
+
+      {(!isSmallScreen || isNavVisible) && (
+        <nav className="navigation">
           {navigation.map((item, index) => (
-            <li className="header-item" key={index}>
-              {item}
-            </li>
+            <h3>
+              <a key={index} href={`#${item}`}>
+                {item}
+              </a>
+            </h3>
           ))}
-        </ul>
-      </h1>
-    </div>
-  </header>
-)
+        </nav>
+      )}
+      <button onClick={toggleNav} className="Burger">
+        🍔
+      </button>
+    </header>
+  )
+}
 
 Header.propTypes = {
   navigation: PropTypes.array,
